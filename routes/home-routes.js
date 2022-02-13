903ec35d2bd87d64588cf176bf365bf3b6b1e62b
Waffles <knharman@gmail.com>
2022-02-12 18:51:45 -0800

    login/sign up screen

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -10,4 +10,24 @@ router.get('/', async (req, res) => {
     });
 });
 
+/**
+ * route for "/dashboard"
+ * dashboard
+ */
+ router.get('/dashboard', async (req, res) => {
+    res.render('dashboard', {
+        // req.session.loggedIn
+    });
+});
+
+/**
+ * route for "/login"
+ * login
+ */
+ router.get('/login', async (req, res) => {
+    res.render('login', {
+        // req.session.loggedIn
+    });
+});
+
 module.exports = router;
