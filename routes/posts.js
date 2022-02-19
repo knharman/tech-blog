049461b80758400a30f8af27b451ec83d38a0d05
Waffles <knharman@gmail.com>
2022-02-19 15:34:37 -0800

    user generated data displaying

--- routes/posts.js
+++ routes/posts.js
@@ -1,21 +1,26 @@
 const Post = require('../models/Post');
-
+const User = require('../models/User');
 const router = require('express').Router();
+const sequelize = require('sequelize');
 
 /**
  * GET
  * /posts/id
  */
 router.get('/:id', async (req, res) => {
-    const post = await Post.findByPk(req.params.id)
+    const post = await Post.findByPk(req.params.id, {
+        include: [
+            User
+        ]
+    }) 
 
     res.render('post', {
         shouldBeAbleToDelete: post.owner_id == req.session.userId,
         loggedIn: req.session.loggedIn, 
-        title: 'Why MVC is so important', 
-        body: 'Here is the post', 
-        username: 'wigg', 
-        date: '2/18/2022', 
+        title: post.title, 
+        body: post.body, 
+        username: post.user.username, 
+        date: post.createdAt,
         id: req.params.id
     });
 })
